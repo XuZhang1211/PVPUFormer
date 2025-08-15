ef91af8529077f9a7221be31db1ebef54ac476b0
XuZhang1211 <anjoyhui@gmail.com>
2025-08-16 02:19:20 +0800

    Release code

--- run_train_edloss.sh
+++
@@ -1,6 +0,0 @@
-export CUDA_VISIBLE_DEVICES=4
-python train.py models/iSegNet/vpu_base448_cocolvis.py \
-    --is-model-path=isegm/model/is_vpu_model.py \
-    --batch-size=12 \
-    --workers=4 \
-    --ngpus=1
